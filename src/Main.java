a03ba7d738d1ed05d4837898bb4a0692f01ce718
Rob Culclasure <robculclasure@Robs-MacBook-Pro.local>
2017-02-20 16:26:48 -0600

    Reduced usage of literal numbers

--- src/Main.java
+++ src/Main.java
@@ -15,44 +15,45 @@ public class Main {
         wordBank.add("emblaze");
         wordBank.add("oblique");
 
-        Integer rand = random.nextInt(4);
+        Integer rand = random.nextInt(wordBank.size());
         String secretWord = wordBank.get(rand);
+        Integer secretWordLength = secretWord.length();
         char[] newWord = "_______".toCharArray();
+
 
         System.out.println("Welcome to Hangman!");
         System.out.println("Guess the letters contained in the secret 7 letter word. You get 5 guesses.");
 
+        Integer guessesAllowed = 5;
         Integer guessesMade = 0;
         Integer correctLetters = 0;
 
-        while(guessesMade < 5 && correctLetters < 7) {
+        while(guessesMade < guessesAllowed && correctLetters < secretWordLength) {
             System.out.println(newWord);
             System.out.println("Enter letter:");
             String userGuess = scanner.nextLine();
             char[] guess = userGuess.toCharArray();
 
             if (secretWord.contains(userGuess)) {
-
-                for (int i = 0; i < 7; i++) {
+                for (int i = 0; i < secretWordLength; i++) {
                     if (secretWord.substring(i, i + 1).contentEquals(userGuess)) {
                         newWord[i] = guess[0];
                         correctLetters++;
                     }
                 }
-                if(correctLetters < 7) {
-                    System.out.println("Good guess. " + (5 - guessesMade) + " guess[es] remaining.");
+                if(correctLetters < secretWordLength) {
+                    System.out.println("Good guess. " + (guessesAllowed - guessesMade) + " guess[es] remaining.");
                 }
-
             } else {
                 guessesMade++;
-                System.out.println("Bad guess. " + (5 - guessesMade) + " guess[es] remaining.");
+                System.out.println("Bad guess. " + (guessesAllowed - guessesMade) + " guess[es] remaining.");
             }
         }
 
-        if(guessesMade > 4){
+        if(guessesMade == guessesAllowed){
             System.out.println("Sorry, out of guesses. The word was " + secretWord + ".");
         }
-        else if(correctLetters ==7){
+        else if(correctLetters == secretWordLength){
             System.out.println("Great job, you guessed the secret word! It was " + secretWord + ".");
         }
 
